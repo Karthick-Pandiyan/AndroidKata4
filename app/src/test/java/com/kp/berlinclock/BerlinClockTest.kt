4b32f2f3501ee9f2fd80fff0821abe1087cfc02b
karthickpandiyan <pandiyan289@gmail.com>
2020-01-06 11:59:25 +0530

    [Created test case] to Test Given verifyTimeFormat function returns invalid format exception when invalid time format passed.

--- app/src/test/java/com/kp/berlinclock/BerlinClockTest.kt
+++ app/src/test/java/com/kp/berlinclock/BerlinClockTest.kt
@@ -14,6 +14,11 @@ class BerlinClockTest {
         verifyTimeFormat("abcde")
     }
 
+    @Test(expected = IllegalArgumentException::class)
+    fun `Given verifyTimeFormat function returns invalid format exception when invalid time format passed`(){
+        verifyTimeFormat("21:30")
+    }
+
     @Throws(ParseException::class)
     fun verifyTimeFormat(time: String) {
         if(TimeValidator.getInstance().validate(time, "HH:mm:ss", Locale.forLanguageTag("de-DE")) == null) {
