f6b4c48b556e352576e3a4c230664c8e581a00fa
karthickpandiyan <pandiyan289@gmail.com>
2020-01-06 15:11:03 +0530

    Integrate the Berlin clock into Activity and consume the Berlin clock usable solution.

--- app/src/main/java/com/kp/berlinclock/MainActivity.kt
+++ app/src/main/java/com/kp/berlinclock/MainActivity.kt
@@ -1,18 +1,33 @@
 package com.kp.berlinclock
 
+import android.annotation.SuppressLint
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
-import android.view.Menu
-import android.view.MenuItem
 
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_main.*
+import java.text.SimpleDateFormat
+import java.util.*
+import android.os.Handler
+
+
+
 
 class MainActivity : AppCompatActivity() {
 
+    @SuppressLint("SimpleDateFormat")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val berlinClock = BerlinClock()
+        val someHandler = Handler(mainLooper)
+        someHandler.postDelayed(object : Runnable {
+            override fun run() {
+                txtBerlinClock.text = berlinClock.transformToBerlinTime(SimpleDateFormat("HH:mm:ss").format(Date(System.currentTimeMillis())))
+                someHandler.postDelayed(this, 1000)
+            }
+        }, 10)
 
     }
 }
