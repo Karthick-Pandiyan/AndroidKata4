45c500364ffe09dcc8253c2180f313fba648452e
karthickpandiyan <pandiyan289@gmail.com>
2020-01-06 11:56:17 +0530

    Created verifyTimeFormat function to test which is returning Exception when it is invalid time format.

--- app/src/test/java/com/kp/berlinclock/BerlinClockTest.kt
+++ app/src/test/java/com/kp/berlinclock/BerlinClockTest.kt
@@ -1,5 +1,16 @@
 package com.kp.berlinclock
 
+import org.apache.commons.validator.routines.TimeValidator
+import java.lang.IllegalArgumentException
+import java.text.ParseException
+import java.util.*
+
 class BerlinClockTest {
 
+    @Throws(ParseException::class)
+    fun verifyTimeFormat(time: String) {
+        if(TimeValidator.getInstance().validate(time, "HH:mm:ss", Locale.forLanguageTag("de-DE")) == null) {
+                throw IllegalArgumentException("Cannot parse time - $time")
+        }
+    }
 }
